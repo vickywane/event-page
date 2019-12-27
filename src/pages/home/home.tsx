7c33caf3b4e300d80f237cacd539cb82bb2e2866
vickywane <vickywane@gmail.com>
2019-12-27 00:04:17 +0000

    more

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Banner from "../../components/head/banner"
+
+const Home = (): JSX.Element => {
+  return (
+    <div>
+      <Banner />
+      <p> Home component here </p>
+    </div>
+  )
+}
+
+export default Home
